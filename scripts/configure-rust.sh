99084f649e25d60f156d53292287db9e3717d09a
YoEight <yo.eight@gmail.com>
2022-12-27 22:51:58 -0500

    Update rust lsp install by using only rustup.

--- scripts/configure-rust.sh
+++ scripts/configure-rust.sh
@@ -3,12 +3,10 @@
 RUST_ANALYZER_VERSION="2022-12-26"
 
 function install:rust() {
-  echo "TODO"  
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 function install:rust:lsp() {
-  wget -O /tmp/rust-analyzer.gz https://github.com/rust-lang/rust-analyzer/releases/download/$RUST_ANALYZER_VERSION/rust-analyzer-x86_64-unknown-linux-gnu.gz
-  gzip -fd /tmp/rust-analyzer.gz
-  cp /tmp/rust-analyzer $LSP_BIN_DIRECTORY
-  chmod u+x $LSP_BIN_DIRECTORY/rust-analyzer
+  rustup component add rust-analyzer
+  ln -s $(rustup which --toolchain stable rust-analyzer) $LSP_BIN_DIRECTORY/rust-analyzer 
 }
